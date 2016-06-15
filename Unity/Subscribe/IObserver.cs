2cfd46208744de91db5eacc23a1f500e53d75762
liuhanwen <liuhanwen@mokylin.com>
2016-06-15 17:52:08 +0800

    Add SO_Patter Base version

---
+++ Unity/Subscribe/IObserver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+/// <summary>
+/// 发布订阅模式-订阅者
+/// </summary>
+public interface IObserver
+{
+
+    string Name { get; set; }
+    
+    /// <summary>
+    /// 订阅者的通知响应
+    /// </summary>
+    /// <param name="subject"></param>
+    void IUpdate( string subject );
+}
---
+++ Unity/Subscribe/Subject.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class Subject : ISubject
+{
+    private static volatile Subject _instance;
+    private static object _lock = new object();
+
+    public static Subject Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock(_lock)
+                {
+                    if (_instance == null) 
+                        _instance = new Subject();
+                }
+            }
+            return _instance;
+        }
+    }
+
+
+    private Dictionary<string, IObserver> dict = new Dictionary<string,IObserver>();
+
+    public Dictionary<string, IObserver> ObserverPool
+    {
+        get { return dict; }
+    }
+
+    private Subject() { }
+
+    public void AddObserver( IObserver observer )
+    {
+        if( !ObserverPool.ContainsKey( observer.Name ) )
+        {
+            ObserverPool.Add( observer.Name, observer );
+            UnityEngine.Debug.Log( "订阅者【添加】成功,现在订阅者数量"+ObserverPool.Count );
+        }
+        else
+        {
+            UnityEngine.Debug.Log( observer.Name + " is already exist" );
+        }
+    }
+
+    public void RemoveObserver( IObserver observer )
+    {
+        if( ObserverPool.ContainsKey( observer.Name ) )
+        {
+            ObserverPool.Remove( observer.Name );
+            UnityEngine.Debug.Log( "订阅者【移除】成功,现在订阅者数量" + ObserverPool.Count );
+        }
+        else
+        {
+            UnityEngine.Debug.Log( "Couldn't find " + observer.Name + " and no observer rmoved" );
+        }
+
+    }
+
+    public void Notify( string subject )
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        //效率会稍微慢点？
+        //IEnumerable<IObserver> jquary = from keyValue in ObserverPool
+        //                                select keyValue.Value;
+
+        //foreach( IObserver item in jquary )
+        //{
+        //    item.IUpdate( subject );
+        //}
+
+
+
+        foreach( var item in ObserverPool )
+        {
+            item.Value.IUpdate( subject );
+        }
+
+        sw.Stop();
+        UnityEngine.Debug.Log( sw.ElapsedMilliseconds );
+    }
+}
