4b8280c010ab2f347c3362ea19a183269b686995
Pasdedeux <lc10278@163.com>
2017-03-24 16:58:11 +0800

    编辑器扩展功能： 添加多项菜单自定义层及显示

---
+++ Unity/Mono/Editor/Editor/PrefabTypeEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 该文件只需放置在Editor文件夹下，不一定是Asset/Editor/
+/// </summary>
+[CustomEditor( typeof( PrefabType ) )]
+[CanEditMultipleObjects]
+public class PrefabTypeEditor : Editor
+{
+    private PrefabType _pt;
+    private SerializedObject _ptObj;
+    private SerializedProperty _type, _propType;
+    private SerializedProperty _goldLevel;
+
+
+    private void OnEnable( )
+    {
+        _ptObj = new SerializedObject( target );
+        _type = _ptObj.FindProperty( "type" );
+        _propType = _ptObj.FindProperty( "propType" );
+        _goldLevel = _ptObj.FindProperty( "goldLevel" );
+    }
+
+    public override void OnInspectorGUI( )
+    {
+        base.OnInspectorGUI();
+
+        serializedObject.Update();
+
+        _pt = ( PrefabType ) target;
+
+        //道具类型包含子菜单
+        if ( _pt.type == PrefabType.InType.Prop )
+        {
+            _pt.propType = ( PrefabType.PropType ) EditorGUILayout.EnumPopup( "PropType:" , _pt.propType );
+
+            //道具类型为金币时显示金币数量
+            if ( _pt.propType == PrefabType.PropType.Gold )
+            {
+                EditorGUILayout.PropertyField( _goldLevel );
+            }
+
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Unity/Mono/Editor/PrefabType.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 预制件类型
+/// </summary>
+public class PrefabType : MonoBehaviour 
+{
+    public enum InType
+    {
+        None,
+        /// <summary>
+        /// 地形
+        /// </summary>
+        Landform,
+        /// <summary>
+        /// 障碍物
+        /// </summary>
+        Obstacle,
+        /// <summary>
+        /// 起点
+        /// </summary>
+        Player,
+        /// <summary>
+        /// 终点
+        /// </summary>
+        Hole,
+        /// <summary>
+        /// 道具
+        /// </summary>
+        Prop,
+    }
+    public InType type;
+
+    public enum PropType
+    {
+        /// <summary>
+        /// 金币
+        /// </summary>
+        Gold,
+        /// <summary>
+        /// 道具1
+        /// </summary>
+        Item1,
+        /// <summary>
+        /// 道具2
+        /// </summary>
+        Item2,
+    }
+    [HideInInspector]
+    public PropType propType;
+
+    [HideInInspector]
+    [Range( 1 , 5 )]
+    public int goldLevel;
+
+}
